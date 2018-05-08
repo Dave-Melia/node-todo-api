a796ce50822ac18efadb2029b7411a4ce535aed3
Dave Melia <retrospock@gmail.com>
2018-05-08 03:47:00 +0100

    add update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5aee82944d8d75d5cb31866d')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5aee5a7694742331c813fe77')
+  }, {
+    $set: {
+      name: 'Dave'
+    },
+    $inc: {
+      age: 2
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+//  db.close();
+});
